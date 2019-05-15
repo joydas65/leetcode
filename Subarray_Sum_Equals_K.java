167f5ba51756b7f046c4cedd8b2f4e9ceb0b24ae
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-05-15 20:07:10 +0530

    Update Subarray_Sum_Equals_K.java

--- Subarray_Sum_Equals_K.java
+++ Subarray_Sum_Equals_K.java
@@ -1,12 +1,18 @@
+import java.util.*;
 class Solution {
     public int subarraySum(int[] nums, int k){
         int c=0,sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum += nums[j];
-                if(sum == k)
-                    c++;
+        HashMap<Integer, Integer> d = new HashMap<Integer, Integer>();
+        d.put(0,1);
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(d.containsKey(sum-k)){
+                c += d.get(sum-k);
+            }
+            if(d.containsKey(sum)){
+                d.put(sum, d.get(sum)+1);
+            }else{
+                d.put(sum, 1);
             }
         }
         return c;
